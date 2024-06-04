4f9d0bdcf154765eba61321666ca59198a66d2f7
Gabriel Beirao <gabrielbei@hotmail.com>
2024-06-04 16:30:08 -0300

    photos added

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -8,7 +8,7 @@ export const getHeader = () => {
 	const token = localStorage.getItem("token")
 	return {
 		Authorization: `Bearer ${token}`,
-		"Content-Type": "application/json"
+		"Content-Type": "multipart/form-data;"
 	}
 }
 
